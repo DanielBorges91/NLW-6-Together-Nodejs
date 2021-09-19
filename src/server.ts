e53f032e403d22b506ddcba0133b3ff55fdd171e
Daniel <danprog91@gmail.com>
2021-09-19 08:48:16 -0300

    Clear code test

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get("/test", (request, response) => {
- return response.json({message: "Hello world!"});
-});
-
-app.post("/test-post", (request, response) => {
- return response.send("Olá Daniel");
-});
-
 app.listen(3000, () => {
  console.log("🚀 Server started on port 3000!");
 });
